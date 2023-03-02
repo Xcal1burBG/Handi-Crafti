92f3fcce1c17e402f0fcbff5b6953b2cec87aae3
Xcal1burBG <ivalkov1979@gmail.com>
2023-03-02 17:29:15 +0200

    Header and Footer components created

--- handicrafti/client/src/App.js
+++ handicrafti/client/src/App.js
@@ -1,8 +1,15 @@
-import './App.css';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+
+      <Header/>
+      <main>
+
+      </main>
+      <Footer/>
      
     </div>
   );
